860e70a3775fc9842b408cc81c0ff04a39c0bcb6
Bret Comnes <bcomnes@gmail.com>
2020-11-03 14:06:55 -0700

    Fix eslint

--- src/date-input-polyfill.js
+++ src/date-input-polyfill.js
@@ -1,23 +1,23 @@
-import './date-input-polyfill.css';
-import Input from './input.js';
+import './date-input-polyfill.css'
+import Input from './input.js'
 
 const addPickers = () => {
-  Input.addPickerToOtherInputs();
+  Input.addPickerToOtherInputs()
   // Check if type="date" is supported.
-  if(!Input.supportsDateInput()) {
-    Input.addPickerToDateInputs();
+  if (!Input.supportsDateInput()) {
+    Input.addPickerToDateInputs()
   }
-};
+}
 
 // Run the above code on any <input type="date"> in the document, also on dynamically created ones.
-addPickers();
+addPickers()
 
-document.addEventListener(`DOMContentLoaded`, () => {
-  addPickers();
-});
+document.addEventListener('DOMContentLoaded', () => {
+  addPickers()
+})
 
 // This is also on mousedown event so it will capture new inputs that might
 // be added to the DOM dynamically.
-document.querySelector(`body`).addEventListener(`mousedown`, () => {
-  addPickers();
-});
+document.querySelector('body').addEventListener('mousedown', () => {
+  addPickers()
+})
--- src/input.js
+++ src/input.js
@@ -1,159 +1,159 @@
-import thePicker from './picker.js';
-import locales from './locales.js';
-import dateFormat from './dateformat.js';
+import thePicker from './picker.js'
+import locales from './locales.js'
+import dateFormat from './dateformat.js'
 
 export default class Input {
-  constructor(input) {
-    this.element = input;
-    this.element.setAttribute(`data-has-picker`, ``);
+  constructor (input) {
+    this.element = input
+    this.element.setAttribute('data-has-picker', '')
 
     this.locale =
-      this.element.getAttribute(`lang`)
-      || document.body.getAttribute(`lang`)
-      || `en`;
+      this.element.getAttribute('lang') ||
+      document.body.getAttribute('lang') ||
+      'en'
 
-    this.format = this.element.getAttribute('date-format')
-      || document.body.getAttribute('date-format')
-      || this.element.getAttribute(`data-date-format`)
-      || document.body.getAttribute(`data-date-format`)
-      || `yyyy-mm-dd`;
+    this.format = this.element.getAttribute('date-format') ||
+      document.body.getAttribute('date-format') ||
+      this.element.getAttribute('data-date-format') ||
+      document.body.getAttribute('data-date-format') ||
+      'yyyy-mm-dd'
 
-    this.localeText = this.getLocaleText();
+    this.localeText = this.getLocaleText()
 
     Object.defineProperties(
       this.element,
       {
-        'valueAsDate': {
+        valueAsDate: {
           get: () => {
-            if(!this.element.value) {
-              return null;
+            if (!this.element.value) {
+              return null
             }
-            const format = this.format || 'yyyy-mm-dd';
-            const parts = this.element.value.match(/(\d+)/g);
-            let i = 0, fmt = {};
+            const format = this.format || 'yyyy-mm-dd'
+            const parts = this.element.value.match(/(\d+)/g)
+            let i = 0; const fmt = {}
 
-            format.replace(/(yyyy|dd|mm)/g, part=> {
-              fmt[part] = i++;
-            });
-            return new Date(parts[fmt['yyyy']], parts[fmt['mm']]-1, parts[fmt['dd']]);
+            format.replace(/(yyyy|dd|mm)/g, part => {
+              fmt[part] = i++
+            })
+            return new Date(parts[fmt.yyyy], parts[fmt.mm] - 1, parts[fmt.dd])
           },
           set: val => {
-            this.element.value = dateFormat(val, this.format);
+            this.element.value = dateFormat(val, this.format)
           }
         },
-        'valueAsNumber': {
-          get: ()=> {
-            if(!this.element.value) {
-              return NaN;
+        valueAsNumber: {
+          get: () => {
+            if (!this.element.value) {
+              return NaN
             }
 
-            return this.element.valueAsDate.valueOf();
+            return this.element.valueAsDate.valueOf()
           },
-          set: val=> {
-            this.element.valueAsDate = new Date(val);
+          set: val => {
+            this.element.valueAsDate = new Date(val)
           }
         }
       }
-    );
+    )
 
     // Open the picker when the input get focus,
     // also on various click events to capture it in all corner cases.
     const showPicker = (e) => {
-      const elm = this.element;
-      elm.locale = this.localeText;
-      const didAttach = thePicker.attachTo(elm);
-    };
-    this.element.addEventListener(`focus`, showPicker);
-    this.element.addEventListener(`mouseup`, showPicker);
+      const elm = this.element
+      elm.locale = this.localeText
+      const didAttach = thePicker.attachTo(elm) /* eslint-disable-line no-unused-vars */
+    }
+    this.element.addEventListener('focus', showPicker)
+    this.element.addEventListener('mouseup', showPicker)
 
     // Update the picker if the date changed manually in the input.
-    this.element.addEventListener(`keydown`, e => {
-      const date = new Date();
+    this.element.addEventListener('keydown', e => {
+      const date = new Date()
 
-      switch(e.keyCode) {
+      switch (e.keyCode) {
         case 9:
         case 27:
-          thePicker.hide();
-          break;
+          thePicker.hide()
+          break
         case 38:
-          if(this.element.valueAsDate) {
-            date.setDate(this.element.valueAsDate.getDate() + 1);
-            this.element.valueAsDate = date;
-            thePicker.pingInput();
+          if (this.element.valueAsDate) {
+            date.setDate(this.element.valueAsDate.getDate() + 1)
+            this.element.valueAsDate = date
+            thePicker.pingInput()
           }
-          break;
+          break
         case 40:
-          if(this.element.valueAsDate) {
-            date.setDate(this.element.valueAsDate.getDate() - 1);
-            this.element.valueAsDate = date;
-            thePicker.pingInput();
+          if (this.element.valueAsDate) {
+            date.setDate(this.element.valueAsDate.getDate() - 1)
+            this.element.valueAsDate = date
+            thePicker.pingInput()
           }
-          break;
+          break
         default:
-          break;
+          break
       }
 
-      thePicker.sync();
-    });
+      thePicker.sync()
+    })
 
-    this.element.addEventListener(`keyup`, e => {
-      thePicker.sync();
-    });
+    this.element.addEventListener('keyup', e => {
+      thePicker.sync()
+    })
   }
 
-  getLocaleText() {
-    const locale = this.locale.toLowerCase();
+  getLocaleText () {
+    const locale = this.locale.toLowerCase()
 
-    for(const localeSet in locales) {
-      const localeList = localeSet.split(`_`);
-      localeList.map(el=>el.toLowerCase());
+    for (const localeSet in locales) {
+      const localeList = localeSet.split('_')
+      localeList.map(el => el.toLowerCase())
 
-      if(
-        !!~localeList.indexOf(locale)
-        || !!~localeList.indexOf(locale.substr(0,2))
+      if (
+        !!~localeList.indexOf(locale) ||
+        !!~localeList.indexOf(locale.substr(0, 2))
       ) {
-        return locales[localeSet];
+        return locales[localeSet]
       }
     }
   }
 
   // Return false if the browser does not support input[type="date"].
-  static supportsDateInput() {
-    const input = document.createElement(`input`);
-    input.setAttribute(`type`, `date`);
+  static supportsDateInput () {
+    const input = document.createElement('input')
+    input.setAttribute('type', 'date')
 
-    const notADateValue = `not-a-date`;
-    input.setAttribute(`value`, notADateValue);
+    const notADateValue = 'not-a-date'
+    input.setAttribute('value', notADateValue)
 
-    return !(input.value === notADateValue);
+    return !(input.value === notADateValue)
   }
 
   // Will add the Picker to all inputs in the page.
-  static addPickerToDateInputs() {
+  static addPickerToDateInputs () {
     // Get and loop all the input[type="date"]s in the page that do not have `[data-has-picker]` yet.
-    const dateInputs = document.querySelectorAll(`input[type="date"]:not([data-has-picker])`);
-    const length = dateInputs.length;
+    const dateInputs = document.querySelectorAll('input[type="date"]:not([data-has-picker])')
+    const length = dateInputs.length
 
-    if(!length) {
-      return false;
+    if (!length) {
+      return false
     }
 
-    for(let i = 0; i < length; ++i) {
-      new Input(dateInputs[i]);
+    for (let i = 0; i < length; ++i) {
+      new Input(dateInputs[i]) /* eslint-disable-line no-new */
     }
   }
 
-  static addPickerToOtherInputs() {
+  static addPickerToOtherInputs () {
     // Get and loop all the input[type="text"] class date-polyfill in the page that do not have `[data-has-picker]` yet.
-    const dateInputs = document.querySelectorAll(`input[type="text"].date-polyfill:not([data-has-picker])`);
-    const length = dateInputs.length;
+    const dateInputs = document.querySelectorAll('input[type="text"].date-polyfill:not([data-has-picker])')
+    const length = dateInputs.length
 
-    if(!length) {
-      return false;
+    if (!length) {
+      return false
     }
 
-    for(let i = 0; i < length; ++i) {
-      new Input(dateInputs[i]);
+    for (let i = 0; i < length; ++i) {
+      new Input(dateInputs[i]) /* eslint-disable-line no-new */
     }
   }
 }
